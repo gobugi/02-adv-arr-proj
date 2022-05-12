6500fe926f18723caf60e605abee872a394b7ae9
Johnny Park <johnpark1987@gmail.com>
2022-05-11 20:55:06 -0600

    first commit

---
+++ problems/04-remove-e-words-filter.js
@@ -0,0 +1,33 @@
+/*
+Write a function `removeEWords(sentence)` that accepts a sentence string as an
+arg. The function should return a new string, containing only the words that
+don't have the letter "e" in them.
+
+Solve this using Array's `filter()` method.
+
+Examples:
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
+
+*/
+
+// your code here
+
+let removeEWords = (sentence) => {
+  let wordsArr = sentence.split(' ');
+
+  let newArr = wordsArr.filter(ele => {
+    return (!ele.includes('e') && !ele.includes('E'))
+  })
+
+  return newArr.join(' ');
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = removeEWords;
+} catch (e) {
+  module.exports = null;
+}
---
+++ problems/11-has-three-vowels.js
@@ -0,0 +1,45 @@
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+
+// your code here
+
+const hasThreeVowels = str => {
+    let newArr = str.split('');
+
+    const vowels = 'aeiou';
+
+    let newVowels = '';
+
+    newArr.forEach(char => {
+        if (vowels.includes(char.toLowerCase) && !newVowels.includes(char.toLowerCase())) {
+            newVowels += char;
+        }
+    })
+
+    return (newVowels.length > 2)
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hasThreeVowels;
+} catch (e) {
+    module.exports = null;
+}
